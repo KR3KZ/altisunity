d59e2addd4c99f885ba4ff42d92c55eafae05022
3rK <ikreekzi@gmail.com>
2019-01-30 16:50:34 +0100

    Modification spawn civ/medic/cop

--- mission-serveur/config/Config_SpawnPoints.hpp
+++ mission-serveur/config/Config_SpawnPoints.hpp
@@ -12,95 +12,53 @@ class CfgSpawnPoints {
 	class Lythium {
 		class Civilian {
 			class spawn1 {
-				displayName = "Kavala";
+				displayName = "Kinduf";
 				spawnMarker = "civ_spawn_1";
-				icon = "\a3\ui_f\data\map\MapControl\watertower_ca.paa";
-				conditions = "!license_civ_rebel";
-			};
-
-			class spawn2 {
-				displayName = "Athira";
-				spawnMarker = "civ_spawn_3";
 				icon = "\a3\ui_f\data\map\MapControl\watertower_ca.paa";
 				conditions = "";
 			};
 
-			class spawn3 {
-				displayName = "Pyrgos";
+			class spawn2 {
+				displayName = "KalaeNoowi";
 				spawnMarker = "civ_spawn_2";
 				icon = "\a3\ui_f\data\map\MapControl\watertower_ca.paa";
 				conditions = "";
 			};
 
-			class spawn4 {
-				displayName = "Sofia";
-				spawnMarker = "civ_spawn_4";
+			class spawn3 {
+				displayName = "Limari";
+				spawnMarker = "civ_spawn_3";
 				icon = "\a3\ui_f\data\map\MapControl\watertower_ca.paa";
 				conditions = "";
-			};
-
-			class spawn5 {
-				displayName = "";
-				spawnMarker = "Rebelop";
-				icon = "\a3\ui_f\data\map\MapControl\bunker_ca.paa";
-				conditions = "license_civ_rebel";
-			};
-
-			class spawn6 {
-				displayName = "";
-				spawnMarker = "Rebelop_1";
-				icon = "\a3\ui_f\data\map\MapControl\bunker_ca.paa";
-				conditions = "license_civ_rebel";
-			};
-
-			class spawn7 {
-				displayName = "";
-				spawnMarker = "Rebelop_2";
-				icon = "\a3\ui_f\data\map\MapControl\bunker_ca.paa";
-				conditions = "license_civ_rebel";
 			};
 		};
 
 		class Cop {
 			class spawn_cop1 {
-				displayName = "Kavala HQ";
+				displayName = "KalaeNoowi";
 				spawnMarker = "cop_spawn_1";
-				icon = "\a3\ui_f\data\map\MapControl\watertower_ca.paa";
+				icon = "\a3\ui_f\data\map\GroupIcons\badge_rotate_0_gs.paa";
 				conditions = "";
 			};
 
 			class spawn_cop2 {
-				displayName = "Athira HQ";
-				spawnMarker = "cop_spawn_3";
-				icon = "\a3\ui_f\data\map\MapControl\fuelstation_ca.paa";
-				conditions = "";
-			};
-
-			class spawn_cop3 {
-				displayName = "Pyrgos HQ";
+				displayName = "Kinduf";
 				spawnMarker = "cop_spawn_2";
 				icon = "\a3\ui_f\data\map\GroupIcons\badge_rotate_0_gs.paa";
 				conditions = "";
 			};
 
-			class spawn_cop4 {
-				displayName = "";
-				spawnMarker = "cop_spawn_4";
-				icon = "\a3\ui_f\data\map\Markers\NATO\b_air.paa";
-				conditions = "call life_coplevel >= 2 && {license_cop_cAir}";
-			};
-
-			class spawn_cop5 {
-				displayName = "";
-				spawnMarker = "cop_spawn_5";
+			class spawn_cop3 {
+				displayName = "MikisFob";
+				spawnMarker = "cop_spawn_3";
 				icon = "\a3\ui_f\data\map\GroupIcons\badge_rotate_0_gs.paa";
-				conditions = "call life_coplevel >= 3";
+				conditions = "";
 			};
 		};
 
 		class Medic {
 			class spawn_med1 {
-				displayName = "";
+				displayName = "KalaeNoowi";
 				spawnMarker = "medic_spawn_1";
 				icon = "\a3\ui_f\data\map\MapControl\hospital_ca.paa";
 				conditions = "";
